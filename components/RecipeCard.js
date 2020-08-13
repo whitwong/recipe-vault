c4dfde35318401e603660152d51df249162f6413
Whitney Wong <whitney.wong@utexas.edu>
2020-08-13 14:45:12 -0500

    Delete Recipe Feature
    Added click handler function to RecipeCard delete icon to delete recipe from db. Changed state reset value for setIngredients() to be consistent with initial state value.

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,7 +1,14 @@
+import db from '../lib/firestore';
 import styles from '../styles/Home.module.css';
 import { MdDelete, MdEdit } from 'react-icons/md';
 
 export default function RecipeCard({ recipes }) {
+  const handleDelete = (id) => {
+    db.collection('recipes')
+      .doc(id)
+      .delete()
+  }
+
   return (
     <>
       {
@@ -13,8 +20,8 @@ export default function RecipeCard({ recipes }) {
               <div className={styles.recipeIngredients}>{recipe.ingredients.join(', ')}</div>
             </div>
             <div>
-              <MdEdit className={styles.recipeUpdate} data-id={recipe.id} />
-              <MdDelete className={styles.recipeDelete} data-id={recipe.id} />
+              <MdEdit className={styles.recipeUpdate} data-id={recipe.id} onClick={() => console.log("Edit ", recipe.id)} />
+              <MdDelete className={styles.recipeDelete} data-id={recipe.id} onClick={() => handleDelete(recipe.id)} />
             </div>
           </div>
         ))
