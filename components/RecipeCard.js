50a49d23e942f2c55e9b3ceb147e4702304b0aa9
Whitney Wong <whitney.wong@utexas.edu>
2020-08-15 19:59:56 -0500

    Moved modals to /pages/recipes/[id].js
    Removed modal components and associated functionality to recipe details page in [id].js.

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,25 +1,7 @@
 import Link from 'next/link';
-import { useState } from 'react';
 import styles from '../styles/Home.module.css';
-import DeleteModal from '../components/DeleteRecipeModal';
-import UpdateModal from '../components/UpdateRecipeModal';
-import { MdDelete, MdEdit } from 'react-icons/md';
-import { useDisclosure } from '@chakra-ui/core';
 
 export default function RecipeCard({ recipes }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [recipeId, setRecipeId] = useState();
-  const [recipeName, setRecipeName] = useState();
-  const [whichModal, setWhichModal] = useState(null);
-
-  // Click handler to designate which modal to open
-  const handleClick = (action, id, recipeName) => {
-    setWhichModal(action);
-    setRecipeId(id);
-    setRecipeName(recipeName);
-    onOpen();
-  }
-
   return (
     <>
       {/* Recipe Cards */}
@@ -33,23 +15,10 @@ export default function RecipeCard({ recipes }) {
                   <div className={styles.recipeTitle}>{recipe.recipe_name}</div>
                   <div className={styles.recipeIngredients}>{recipe.ingredients.join(', ')}</div>
                 </div>
-                <div>
-                  <MdEdit className={styles.recipeUpdate} data-id={recipe.id} onClick={() => handleClick("update", recipe.id)} />
-                  <MdDelete className={styles.recipeDelete} data-id={recipe.id} onClick={() => handleClick("delete", recipe.id, recipe.recipe_name)} />
-                </div>
               </div> 
             </a>
           </Link>           
         ))
-      }
-
-      {/* Delete/Update Modals */}
-      {
-        whichModal === "delete" ? 
-          <DeleteModal isOpen={isOpen} onClose={onClose} id={recipeId} recipeName={recipeName} /> : 
-        whichModal === "update" ? 
-          <UpdateModal isOpen={isOpen} onClose={onClose} id={recipeId} /> : 
-        null
       }
     </>
   )
